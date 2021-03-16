94930727047b25867bb090830dc7a53a927a31f1
Navid Ahrary <navid.ahrary@outlook.com>
2021-03-16 20:09:52 +0330

    Get start page number from cli

--- index.js
+++ index.js
@@ -3,20 +3,21 @@
 const puppeteer = require('puppeteer-core')
 const Fs = require('fs')
 
-const url = 'https://ganjoor.net/moulavi/shams/ghazalsh/sh1/'
 
 function getPoems () {
   const m1 = document.querySelectorAll('div.m1');
   const m2 = document.querySelectorAll('div.m2');
   
   const poems = [];
+
   m1.forEach(m => poems.push(m.innerText))
   m2.forEach(m => poems.push(m.innerText))
-
+  
   return poems
 }
 
-async function app () {
+async function app (pageNumber = 1) {
+  const url = `https://ganjoor.net/moulavi/shams/ghazalsh/sh${pageNumber}/`;
   const config  = {
     ignoreHTTPSErrors: true,
     args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
@@ -25,31 +26,31 @@ async function app () {
     defaultViewport:{height: 760, width:1366},
     headless: true,
   };
-  const browser = await puppeteer.launch(config)
+  const browser = await puppeteer.launch(config);
   const page = await browser.newPage();
   await page.goto(url);
   
-  let poems = ''
-  let p = 1
+  let poems = '';
   while(true) {
     const result = await page.evaluate(getPoems);
-    poems += result.join('\n')
+
+    poems += result.join('\n') + '\n';
+
+    Fs.appendFileSync('poems.txt', poems);
     
-    console.log(`#${p++} got`)
+    console.log(`Page #${pageNumber++} got`);
     
-    // save to file
-    Fs.appendFileSync('poems.txt', poems)
-    
-    const nextButton = await page.$('div.navleft')
+    const nextButton = await page.$('div.navleft');
     if(!Boolean(nextButton)) {
       break;
     }
 
-    await nextButton.click()
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'})
+    await nextButton.click();
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
   } 
 
   await browser.close();
 };
 
-app();
+const startPageNon = +(process.argv[2] ?? 1)
+app(+startPageNon);
