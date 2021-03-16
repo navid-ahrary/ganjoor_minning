490663a11db2c700addc137d5803d54d49d1f4a9
Navid Ahrary <navid.ahrary@outlook.com>
2021-03-16 19:55:14 +0330

    Save result in file

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict'
 
 const puppeteer = require('puppeteer-core')
+const Fs = require('fs')
 
 const url = 'https://ganjoor.net/moulavi/shams/ghazalsh/sh1/'
+
+function getPoems () {
+  const m1 = document.querySelectorAll('div.m1');
+  const m2 = document.querySelectorAll('div.m2');
+  
+  const poems = [];
+  m1.forEach(m => poems.push(m.innerText))
+  m2.forEach(m => poems.push(m.innerText))
+
+  return poems
+}
 
 async function app () {
   const config  = {
@@ -11,12 +23,30 @@ async function app () {
     dumpio: false,
     executablePath: '/usr/bin/vivaldi',
     defaultViewport:{height: 760, width:1366},
-    headless: false,
+    headless: true,
   };
   const browser = await puppeteer.launch(config)
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({ path: 'example.png' });
+  
+  let poems = ''
+
+  while(true) {
+    const result = await page.evaluate(getPoems);
+
+    poems += result.join('\n')
+    
+    Fs.appendFileSync('poems.txt', poems)
+    
+    const nextButton = await page.$('div.navleft')
+    
+    if(!Boolean(nextButton)) {
+      break;
+    }
+
+    await nextButton.click()
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'})
+  } 
 
   await browser.close();
 };
