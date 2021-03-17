a08909a123d80625aab88eb8a008427c49a1c482
Navid Ahrary <navid.ahrary@outlook.com>
2021-03-17 15:45:39 +0330

    Find a chromium browser auto

---
+++ helpers.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Util = require('util');
+const Exec = Util.promisify(require('child_process').exec);
+
+async function findBrowserPath () {
+  try {
+    const res = await Exec('which edge');
+
+    console.log(' Using the Microsoft Edge\n');
+
+    return res.stdout.replace('\n', '');
+  } catch(err) {
+    try {
+      const res = await Exec('which chrome');
+
+      console.log(' Using the Google Chrome\n');
+
+      return res.stdout.replace('\n', '');
+    } catch(err) {
+      try {
+        const res = await Exec('which brave');
+
+        console.log(' Using the Brave\n');  
+
+        return res.stdout.replace('\n', '');
+      }catch(err) {
+        try {
+          const res = await Exec('which chromium');
+
+          console.log('Using the Chromium\n');
+
+          return res.stdout.replace('\n', '');
+        } catch(err) {
+          try {
+
+            const res = await Exec('which vivaldi');
+    
+            console.log(' Using the Vivaldi\n');        
+
+            return res.stdout.replace('\n', '');
+          }catch(err) {
+            throw 'Chromium browser not found'
+          }
+        }
+      }
+    }
+  }
+}
+
+
+module.exports = {
+  findBrowserPath: findBrowserPath
+}
